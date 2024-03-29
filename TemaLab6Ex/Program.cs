c8d61f1665368aad04ea679a5d00502594b5dc6e
Gabriel <Gabriel@GABRIEL>
2024-03-29 15:49:46 +0200

    Add project files.

---
+++ TemaLab6Ex/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Ex 1
+Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o
+metoda care va calcula volumul acestuia.
+Folositi o clasa care va modela un dulap.
+ Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+ Cele trei campuri vor fi initializate cu ajutorul constructorului
+ Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si
+afisati rezultatul.*/
+
+
+
+Closet myCloset = new Closet(2, 3, 4);
+
+int volume = myCloset.CalculateVolume();
+Console.WriteLine("The volume of the closet is: " + volume);
+
+
+class Closet
+{
+    private int length;
+    private int width;
+    private int height;
+
+    public Closet(int length, int width, int height)
+    {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int CalculateVolume()
+    {
+        int volume = length * width * height;
+        return volume;
+    }
+}
+
+
---
+++ TemaLab6Ex2/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Scrieti un program care va modela un autoturism. Un autoturism va avea o
+marca (string), un numar de inmatriculare (string), precum si o
+capacitate cilindrica(int).
+Autoturismul va avea:
+ Campurile corespunzatoare caracteristicilor autoturismului.
+ Un constructor care va initializa campurile acestuia
+ O metoda “Tiparire” care va afisa pe consola descrierea acestuia sub forma : “marca,
+numarDeInmatriculare, capacitateCilindrica”.
+Folositi: clase, campuri, metode, modificatorii de acces adecvati, comentarii
+xml pentru metodele publice.
+Creeati doua obiecte de tipul autoturism in functia “Main” si apelati-le
+metodele “Tipareste”
+*/
+
+
+Car car1 = new Car("Audi", "bh06lab", 2500);
+Car car2 = new Car("Peugeot", "bh02lex", 2000);
+
+car1.Print();
+car2.Print();
+
+class Car
+{
+    private string brand;
+    private string registrationNumber;
+    private int engineCapacity;
+   
+    /// <summary>
+    /// Car Caracteristics
+    /// </summary>
+    /// <param name="brand"></param>
+    /// <param name="registrationNumber"></param>
+    /// <param name="engineCapacity"></param>
+    public Car(string brand, string registrationNumber, int engineCapacity)
+    {
+        this.brand = brand;
+        this.registrationNumber = registrationNumber;
+        this.engineCapacity = engineCapacity;
+    }
+
+    /// <summary>
+    /// Prints the description of the car.
+    /// </summary>
+    public void Print()
+    {
+        Console.WriteLine($"{brand}, {registrationNumber}, {engineCapacity}");
+    }
+}
+
+
+        
+        
